9e923d556af962918816349a6c9ab395813b130d
Isaac Peters <dabomb11111@gmail.com>
2018-10-09 23:22:55 -0700

    added a comment

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -19,6 +19,8 @@ public class Board {
 		// I'm assuming non-zero indexing for the numbers, and valid characters to be 'A' through 'J'
 		// So the board starts at 1-A and ends at 10-J
 
+		//TODO make sure ships don't get placed on top of each other
+
 		if (isVertical) {
 			// Ship is vertical so we make sure it has space above it and is within bounds horizontally
 			if ( (x > 0 && x <= 10) && ((y - 'A') >= 0 && (y - 'A') < (10 - ship.getLength() + 1))) {
