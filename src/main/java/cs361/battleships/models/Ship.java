80cd7ac9e29243c5e211da5ac9cce72cee40bb6a
Isaac Peters <dabomb11111@gmail.com>
2018-10-09 21:18:35 -0700

    Introductory ship stuff

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -2,12 +2,12 @@ package cs361.battleships.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
-
 import java.util.List;
 
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
+	private String shipType;
 
 	public Ship() {
 		occupiedSquares = new ArrayList<>();
@@ -15,6 +15,21 @@ public class Ship {
 	
 	public Ship(String kind) {
 		//TODO implement
+		this.shipType = kind;
+		/*switch(kind) {
+			case "MINESWEEPER":
+				//stuff
+				break;
+
+			case "DESTROYER":
+				//stuff
+				break;
+
+			case "BATTLESHIP":
+				//stuff
+				break;
+
+		}*/
 	}
 
 	public List<Square> getOccupiedSquares() {
