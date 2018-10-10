f6d70ba489560ee1dc0dac70f528992ac71c2b8e
Isaac Peters <dabomb11111@gmail.com>
2018-10-09 22:48:43 -0700

    Can place ships
    Logic for whether or not a ship placement is valid is implemented. The placement of the ships are not stored, however

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -16,7 +16,21 @@ public class Board {
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public boolean placeShip(Ship ship, int x, char y, boolean isVertical) {
-		// TODO Implement
+		if (isVertical) {
+			// Ship is vertical so we make sure it has space above it and is within bounds horizontally
+			if ( (x > 0 && x <= 10) && ((y - 'A') >= 0 && (y - 'A') < (10 - ship.getLength() + 1))) {
+				// We are within bounds, so everything is OK to place the ship
+				// TODO place the ship
+				return true;
+			}
+		} else {
+			// Ship is horizontal so we make sure it has space to the right and is within bounds vertically
+			if ( ((y - 'A') >= 0 && (y - 'A') < 10) && (x > 0 && x <= (10 - ship.getLength() + 1))) {
+				// We are within bounds, so everything is OK to place the ship
+				// TODO place the ship
+				return true;
+			}
+		}
 		return false;
 	}
 
--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -7,33 +7,40 @@ import java.util.List;
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
-	@JsonProperty private String shipType;
+	@JsonProperty private int shipLength;
 
 	public Ship() {
 		occupiedSquares = new ArrayList<>();
 	}
 	
 	public Ship(String kind) {
-		//TODO implement
-		//this.shipType = kind;
-		/*switch(kind) {
+
+		// This switch statement will determine how long our ship is, based on the type string given
+		switch(kind) {
 			case "MINESWEEPER":
-				//stuff
+				this.shipLength = 2;
 				break;
 
 			case "DESTROYER":
-				//stuff
+				this.shipLength = 3;
 				break;
 
 			case "BATTLESHIP":
-				//stuff
+				this.shipLength = 4;
 				break;
 
-		}*/
+		}
+
+//		for(int i = 0; i < this.shipLength; i++) {
+//			this.occupiedSquares.add(new Square());
+//		}
 	}
 
 	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
+		return this.occupiedSquares;
+	}
+
+	public int getLength() {
+		return shipLength;
 	}
 }
