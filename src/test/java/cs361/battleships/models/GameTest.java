e4d998f4ff78b8c03a7c07d5ce8f09230eff4062
Hartwelg <43598755+Hartwelg@users.noreply.github.com>
2018-10-10 21:03:44 -0700

    Update src/test/java/cs361/battleships/models/GameTest.java

--- src/test/java/cs361/battleships/models/GameTest.java
+++ src/test/java/cs361/battleships/models/GameTest.java
@@ -1,12 +1,15 @@
 package cs361.battleships.models;
 
 import org.junit.Test;
+import java.util.Random;
 
 public class GameTest {
 
     @Test
     public void testRandVertical{
-
+    Board board = new Board();
+    Game game = new Game();
+    game.
     }
     @Test
     public void testRandRow{
