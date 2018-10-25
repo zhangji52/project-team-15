243446a1c3704230127c61511908da585139c2c6
amayaevan <43584732+amayaevan@users.noreply.github.com>
2018-10-25 00:48:57 -0700

    Updated string passed to textbox
    clears textarea when game is over and says who won

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -26,7 +26,7 @@ function markHits(board, elementId, surrenderText) {
         else if (attack.result === "SUNK")
             className = "hit"
         else if (attack.result === "SURRENDER")
-            document.getElementById("textBox").value += surrenderText.toString();
+            document.getElementById("textBox").value = surrenderText.toString();
         document.getElementById(elementId).rows[attack.location.row-1].cells[attack.location.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add(className);
     });
 }
@@ -43,8 +43,8 @@ function redrawGrid() {
     game.playersBoard.ships.forEach((ship) => ship.occupiedSquares.forEach((square) => {
         document.getElementById("player").rows[square.row-1].cells[square.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add("occupied");
     }));
-    markHits(game.opponentsBoard, "opponent", "You won the game");
-    markHits(game.playersBoard, "player", "You lost the game");
+    markHits(game.opponentsBoard, "opponent", "You won the game!");
+    markHits(game.playersBoard, "player", "You lost the game :(");
 }
 
 var oldListener;
@@ -88,7 +88,7 @@ function sendXhr(method, url, data, handler) {
     var req = new XMLHttpRequest();
     req.addEventListener("load", function(event) {
         if (req.status != 200) {
-            document.getElementById("textBox").value+="Invalid action\n"
+            document.getElementById("textBox").value +="Invalid action\n"
             return;
         }
         handler(JSON.parse(req.responseText));
