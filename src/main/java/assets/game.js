c4bc65bc7ed6412fac3d024de82b63821d37654c
amayaevan <43584732+amayaevan@users.noreply.github.com>
2018-10-25 19:07:39 -0700

    Changed textbox  to overwrite
    changed the functions piped into textbox to overwrite what is in it instead of adding to it.

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -43,8 +43,8 @@ function redrawGrid() {
     game.playersBoard.ships.forEach((ship) => ship.occupiedSquares.forEach((square) => {
         document.getElementById("player").rows[square.row-1].cells[square.column.charCodeAt(0) - 'A'.charCodeAt(0)].classList.add("occupied");
     }));
-    markHits(game.opponentsBoard, "opponent", "You won the game!");
-    markHits(game.playersBoard, "player", "You lost the game :(");
+    markHits(game.opponentsBoard, "opponent", "You won the game!\n");
+    markHits(game.playersBoard, "player", "You lost the game :(\n");
 }
 
 var oldListener;
@@ -88,7 +88,7 @@ function sendXhr(method, url, data, handler) {
     var req = new XMLHttpRequest();
     req.addEventListener("load", function(event) {
         if (req.status != 200) {
-            document.getElementById("textBox").value +="Invalid action\n"
+            document.getElementById("textBox").value ="Invalid action\n"
             return;
         }
         handler(JSON.parse(req.responseText));
